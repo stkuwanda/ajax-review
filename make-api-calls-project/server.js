365972feee94f5260e0eef3bad38c19196ef75a2
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-26 17:26:10 +0200

    executed a client-side XHR request to retrieve a text file from a local server

--- make-api-calls-project/server.js
+++ make-api-calls-project/server.js
@@ -4,10 +4,16 @@ const http = require('node:http');
 const path = require('node:path');
 
 const html = fs.readFileSync(path.join(__dirname, 'public', 'ajax.html'));
+const txt = fs.createReadStream(path.join(__dirname, 'public', 'sample.txt'));
 
 function get(res) {
 	res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(html);
+}
+
+function getText(res) {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+  txt.pipe(res);
 }
 
 function error(res, code) {
@@ -16,6 +22,11 @@ function error(res, code) {
 }
 
 http.createServer(function (req, res) {
+  if (req.method === 'GET' && req.url === '/sample.txt') {
+    getText(res);
+    return;
+  }
+
   if (req.method === 'GET' && req.url === '/') {
     get(res);
     return;
